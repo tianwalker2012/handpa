a65e10d441eb397e722a416e464d8d7beb5a0ea0
tianwalker2012 <sunnyskyunix@gmail.com>
2013-07-31 10:35:06 +0800

    fix the user confuse

--- msghandler.py
+++ msghandler.py
@@ -69,25 +69,25 @@ def combineImageText(img1, img2):
     return img1.author.name+"&"+img2+"\n"+formatDate(img1.created_at)+"&"+formatDate(img2.created_at)+"\n"+img1.locLabel+"&"+img2.locLabel
 
 def generateCombineImage(img, msg, appOpenID):
-    user = img.author    
-    print 'Recieved image from user:', user.name,",url:",msg['PicUrl']
+    inUser = img.author    
+    print 'Recieved image from user:', inUser.name,",url:",msg['PicUrl']
     #myimage = downloadImage(msg['PicUrl'])
     #matchedImage = getMatchedImage(user)
     setupImageLocation(img, msg)
-    matchedResult = getMatchedImage(user)
+    matchedResult = getMatchedImage(inUser)
     matchedUser = matchedResult.author
     matchedURL = matchedResult.url
     combinedURL = handleImageURL(img.url, matchedURL, 0)
     iconURL = handleImageURL(img.url, matchedURL, 1)
     #
     #1. toID, 2.fromID, createTime,title,content,smallImage,imageURl
-    return combineImageResponse % (user.openid,appOpenID,getCurrentMillis(),user.name+"和"+matchedUser.name+"的合影",combineImageText(img, matchedResult),iconURL, combinedURL)
+    return combineImageResponse % (inUser.openid,appOpenID,getCurrentMillis(),inUser.name+"和"+matchedUser.name+"的合影",combineImageText(img, matchedResult),iconURL, combinedURL)
 
-def getMatchedImage(user):
+def getMatchedImage(inUser):
     """Get an image can match this user, keep it simple and stupid"""
     if len(uploadedImages) > 0:
         for img in reversed(uploadedImages):
-            if user.openid != img.author.openid:
+            if inUser.openid != img.author.openid:
                 return img
     dfImage = Image()
     dfImage.author = user('openid')
@@ -95,10 +95,10 @@ def getMatchedImage(user):
     dfImage.url = 'http://www.enjoyxue.com/static/IMG_0493.JPG'
     return dfImage
 
-def storeUploadImage(url, user, msg):
+def storeUploadImage(url, inUser, msg):
     """Will store a image into memory"""
     img = Image()
-    img.author = user
+    img.author = inUser
     #img.created_at = datetime.now()
     #img.locLabel = user.locLabel
     img.url = url
@@ -106,46 +106,45 @@ def storeUploadImage(url, user, msg):
     uploadedImages.append(img)
     return img
  
-def handle(msg, user):
+def handle(msg, inUser):
     """All the msg will be handled by me. all the logic will changed here"""
     appOpenID = msg.get('ToUserName', None)
-    print "current status:",user.status,",msg type:",msg['MsgType']
-    user.updated_at = datetime.now()
-    if user.status == 1:
+    print "current status:",inUser.status,",msg type:",msg['MsgType']
+    inUser.updated_at = datetime.now()
+    if inUser.status == 1:
         if msg['MsgType'] == 'event':
             #I assume only subscribtion will have event.    
-            user.status = 1
-            print 'new user subscribe us, id:', user.openid
-            return textResponse % (user.openid,appOpenID,getCurrentMillis(),"""感谢使用羽毛相机。发送照片到羽毛相机，瞬间抓到一起拍照的小伙伴，体验照片空中合体。请先输入微信号码（不是中文昵称）完成注册：""")
+            inUser.status = 1
+            print 'new user subscribe us, id:', inUser.openid
+            return textResponse % (inUser.openid,appOpenID,getCurrentMillis(),"""感谢使用羽毛相机。发送照片到羽毛相机，瞬间抓到一起拍照的小伙伴，体验照片空中合体。请先输入微信号码（不是中文昵称）完成注册：""")
         elif msg['MsgType'] == 'text':
-            user.status = 2
-            print 'recived user name:',user.openid,", name:",msg.get('Content', None)
-            user.name = msg.get('Content', None)
-            return textResponse % (user.openid,appOpenID,getCurrentMillis(),"""为保证微信号码准确，请再输入一次：""")
+            inUser.status = 2
+            print 'recived user name:',inUser.openid,", name:",msg.get('Content', None)
+            inUser.name = msg.get('Content', None)
+            return textResponse % (inUser.openid,appOpenID,getCurrentMillis(),"""为保证微信号码准确，请再输入一次：""")
         else:
             print 'revieved none text message'
-            return textResponse % (user.openid, appOpenID,getCurrentMillis(), """请先输入微信号码（不是中文昵称）完成注册：""")
-    elif user.status == 2:
+            return textResponse % (inUser.openid, appOpenID,getCurrentMillis(), """请先输入微信号码（不是中文昵称）完成注册：""")
+    elif inUser.status == 2:
         if msg['MsgType'] == 'text':
-            user.status = 3
-            user.status = 3
-            print 'I will change status to 3, actual status:', user.status
-            return textResponse % (user.openid, appOpenID,getCurrentMillis(), """注册成功。请试拍你的第一张羽毛照片。""")
+            inUser.status = 3
+            print 'I will change status to 3, actual status:', inUser.status
+            return textResponse % (inUser.openid, appOpenID,getCurrentMillis(), """注册成功。请试拍你的第一张羽毛照片。""")
         else:
-            return textResponse % (user.openid,appOpenID,getCurrentMillis(),"""为保证微信号码准确，请再输入一次：""")
-    elif user.status == 3:
+            return textResponse % (inUser.openid,appOpenID,getCurrentMillis(),"""为保证微信号码准确，请再输入一次：""")
+    elif inUser.status == 3:
         print 'I am in status:', user.status,",my type:",msg['MsgType']
         if msg['MsgType'] == 'image':
-            img = storeUploadImage(msg['PicUrl'],user,msg)
-            if user.pendingImage == None:
-                user.pendingImage = img
-                return textResponse % (user.openid, appOpenID,getCurrentMillis(), """拍摄成功！请发送你的地点，或发送任何文字忽略地点。""")
-        if user.pendingImage:
-            pendingImage = user.pendingImage
-            user.pendingImage = None
+            img = storeUploadImage(msg['PicUrl'],inUser,msg)
+            if inUser.pendingImage == None:
+                inUser.pendingImage = img
+                return textResponse % (inUser.openid, appOpenID,getCurrentMillis(), """拍摄成功！请发送你的地点，或发送任何文字忽略地点。""")
+        if inUser.pendingImage:
+            pendingImage = inUser.pendingImage
+            inUser.pendingImage = None
             return generateCombineImage(pendingImage, msg, appOpenID)
         else:
-            return textResponse % (user.openid, appOpenID,getCurrentMillis(), """试拍一张羽毛照片。""")
+            return textResponse % (inUser.openid, appOpenID,getCurrentMillis(), """试拍一张羽毛照片。""")
       
 if __name__ == "__main__":
  userGirl = user("coolid");
